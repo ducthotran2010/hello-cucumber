5b2968ee059533f9235c4b19144614676cdecd6c
Duc Tho Tran <ducthotran2010@gmail.com>
2018-10-22 22:21:33 +0700

    Add something to test

---
+++ src/caculator.js
@@ -0,0 +1,11 @@
+const plus = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const minus = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+module.exports = { 
+    plus,
+    minus, 
+    multiply, 
+    divide,
+}
